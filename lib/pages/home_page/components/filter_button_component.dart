5745992efdbc3a8c0a5212dc957680ded61127da
Jesse <jessesouzaall@gmail.com>
2023-01-13 16:27:58 -0300

    quase finalizando os componentes da primeira tela

---
+++ lib/pages/home_page/components/filter_button_component.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class FilterButtonComponent extends StatefulWidget {
+  const FilterButtonComponent({
+    super.key,
+  });
+
+  @override
+  State<FilterButtonComponent> createState() => _FilterButtonComponentState();
+}
+
+class _FilterButtonComponentState extends State<FilterButtonComponent> {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return SizedBox(
+      height: 70,
+      width: screenSize.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.only(
+              left: 27,
+              top: 25,
+              bottom: 5,
+            ),
+            padding: const EdgeInsets.only(
+              right: 5,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: const Color(0xFFFF5F50),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite, color: Colors.white),
+                  ),
+                  const Text(
+                    'Filter',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/pages/home_page/my_home_page.dart
+++ lib/pages/home_page/my_home_page.dart
@@ -1,14 +1,33 @@
+import 'package:desafio_2/pages/home_page/components/pet_item_list_component.dart';
 import 'package:flutter/material.dart';
 
 import 'components/app_bar.dart';
+import 'components/filter_button_component.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(),
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: const MyAppBar(),
+      body: SizedBox(
+        height: screenSize.height,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: const Color(0xFFF2F2F2),
+          ),
+          child: Column(
+            children: const [
+              FilterButtonComponent(),
+              SizedBox(height: 10),
+              PetItemListComponent()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
